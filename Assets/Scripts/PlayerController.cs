af74a31a8b2d993cb97e74562f765cb0c4ee162f
ZackaryHowell <56128942+ZackaryHowell@users.noreply.github.com>
2019-11-26 10:07:38 -0800

    Food and Animal Movment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayeController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +26,11 @@ public class PlayeController : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
