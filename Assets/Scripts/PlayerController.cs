c1ea7d982da4992b9893cdfa5757a0ee144d3063
ZackaryHowell <56128942+ZackaryHowell@users.noreply.github.com>
2019-12-04 10:03:10 -0800

    Food destroys both animal and food on impact and game over

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 50.0f;
+    public float speed = 10.0f;
     public float xRange = 30;
     public GameObject projectilePrefab;
 
