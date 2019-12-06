a48661b6e52986709f5cdfa97897cc45074ae510
ZackaryHowell <56128942+ZackaryHowell@users.noreply.github.com>
2019-12-06 10:09:55 -0800

    Spawn Time

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 10;
     private float spawnPosZ = 20;
-    private float startDelay = 2;
+    private float startDelay = 1.0f;
     private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
